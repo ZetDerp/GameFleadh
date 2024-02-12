b473a4d2a5aebe4770d4f268f416441515036b87
Jake Fitzgerald <elloofficer@gmail.com>
2024-02-12 16:17:48 +0000

    Setup up basic files, such as Player template and Game Manager.
    Will need to setup preferences in the project's settings next.

---
+++ GamesFleadh/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // This script we will use to :
+    // set the state of the game (game over/ game win)
+    // set the score
+    // can be used to handle UI and update
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GamesFleadh/Assets/Scripts/Test_Trigger_Script.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Trigger_Script : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.CompareTag<" string name ">() == " string name" )
+        //{
+        //    // It's triggered!
+        //}
+    }
+}
