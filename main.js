0a1ee2cce97b66f4549b7f2800acd2d9fbfb241d
Jake Fitzgerald <elloofficer@gmail.com>
2024-02-27 17:55:07 +0000

    PWA 7

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(registration =>
-      {
-        console.log("SW Registered");
-        console.log(registration);
-      }).catch(error => {
-        console.log("SW Registration Failed");
-        console.log(error)
-      })
+    navigator.serviceWorker.register('./sw.js');
   }
 }
--- sw.js
+++ sw.js
@@ -1,57 +1,51 @@
-var GHPATH = '/GameFleadh/';
-var APP_PREFIX = 'astro';
-var VERSION = 'version_002';
-var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/assets/css/styles.css`,
-  `${GHPATH}/assets/img/icon-192-192.png`,
-  `${GHPATH}/assets/img/icon-256-256.png`,
-  `${GHPATH}/assets/img/icon-384-384.png`,
-  `${GHPATH}/assets/img/icon-512-512.png`,
-  `${GHPATH}/assets/js/declareVars.js`
-  `${GHPATH}/assets/js/gameInput.js`
-  `${GHPATH}/assets/js/main.js`
-]
+const cacheName = 'GameFleadh';
+const filesToCache = [
+  '.',
+  'main.js',
+  'index.html',
+  'assets/css/screen.css',
+  './GameFleadh/',
+  './GameFleadh/main.js',
+  './GameFleadh/index.html',
+  './GameFleadh/assets/css/screen.css',
 
-var CACHE_NAME = APP_PREFIX + VERSION
-self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { 
-        console.log('Responding with cache : ' + e.request.url);
-        return request
-      } else {       
-        console.log('File is not cached, fetching : ' + e.request.url);
-        return fetch(e.request)
-      }
-    })
-  )
-})
+];
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Installing cache : ' + CACHE_NAME);
-      return cache.addAll(URLS)
-    })
-  )
-})
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(filesToCache);
+  return self.skipWaiting();
+});
 
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      cacheWhitelist.push(CACHE_NAME);
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('Deleting cache : ' + keyList[i] );
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
-})
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req.url);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
